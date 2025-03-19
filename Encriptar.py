7adb695004d70396d3270a1063ee64b423c7ab5b
Mateus Amaral da Silva <114357991+MateusAmaralDaSilva@users.noreply.github.com>
2025-03-19 17:01:13 -0300

    Update Encriptar.py

--- Encriptar.py
+++ Encriptar.py
@@ -1,1 +1,18 @@
 
+import random
+
+entrada = input("Insira um entrada: ")
+texto = " "
+alfabeto = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+chave = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+random.shuffle(chave)
+
+for letra in entrada:
+    if letra == " ":
+        texto += " "
+    else:
+        texto += (chave[alfabeto.index(letra)])
+        
+print(f"mensagem criptografada: {texto}  chave: {chave}")
+
+
